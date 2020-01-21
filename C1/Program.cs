992c3ae7660aa03b3d29fe5a2c852fa182c2e220
josuesr1992 <59772500+josuesr1992@users.noreply.github.com>
2020-01-21 09:11:22 -0600

    Add project files.

---
+++ C1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C1
+{
+    static class Program
+    {
+        
+
+
+        /*josue 
+         * 
+         */
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //variables
+            string nombre = "josue sandi";
+            int num1 = 450; //entero
+            uint num2 = 500; //entero solo +
+            float num3 = 35.5f; //flotante
+            double num4 = 45.50; //decimales
+            decimal num5 = 502005.45454845m; //numeros grandes con decimales
+            byte num6 = 255; //maximo 255
+            bool acceso = true; //true and false
+
+            DateTime date = DateTime.Today;
+             
+
+            //MessageBox.Show(nombre);
+            ///MessageBox.Show(nombre,num5.ToString());
+            MessageBox.Show(date.ToShortDateString().ToString());
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+        }
+    }
+}
