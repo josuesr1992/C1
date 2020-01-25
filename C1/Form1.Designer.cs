20d4138c84cb77425e77809e8c91051cb71fa9db
josuesr1992 <59772500+josuesr1992@users.noreply.github.com>
2020-01-25 12:56:33 -0600

    program

--- C1/Form1.Designer.cs
+++ C1/Form1.Designer.cs
@@ -28,21 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.bt_ok = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // bt_ok
+            // 
+            this.bt_ok.Location = new System.Drawing.Point(191, 279);
+            this.bt_ok.Name = "bt_ok";
+            this.bt_ok.Size = new System.Drawing.Size(75, 23);
+            this.bt_ok.TabIndex = 0;
+            this.bt_ok.Text = "OK";
+            this.bt_ok.UseVisualStyleBackColor = true;
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(338, 279);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 1;
+            this.btn_close.Text = "close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome Back";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(235, 66);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(155, 20);
+            this.txt_name.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.bt_ok);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bt_ok;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_name;
     }
 }
 
--- C1/Program.cs
+++ C1/Program.cs
@@ -9,7 +9,7 @@ namespace C1
     static class Program
     {
         
-
+        //save
 
         /*josue 
          * 
@@ -21,26 +21,27 @@ namespace C1
         static void Main()
         {
 
-            //variables
-            string nombre = "josue sandi";
-            int num1 = 450; //entero
-            uint num2 = 500; //entero solo +
-            float num3 = 35.5f; //flotante
-            double num4 = 45.50; //decimales
-            decimal num5 = 502005.45454845m; //numeros grandes con decimales
-            byte num6 = 255; //maximo 255
-            bool acceso = true; //true and false
+            ////variables
+            //string nombre = "josue sandi";
+            //int num1 = 450; //entero
+            //uint num2 = 500; //entero solo +
+            //float num3 = 35.5f; //flotante
+            //double num4 = 45.50; //decimales
+            //decimal num5 = 502005.45454845m; //numeros grandes con decimales
+            //byte num6 = 255; //maximo 255
+            //bool acceso = true; //true and false
+            ////prueba 
 
-            DateTime date = DateTime.Today;
+            //DateTime date = DateTime.Today;
              
 
-            //MessageBox.Show(nombre);
-            ///MessageBox.Show(nombre,num5.ToString());
-            MessageBox.Show(date.ToShortDateString().ToString());
+            ////MessageBox.Show(nombre);
+            /////MessageBox.Show(nombre,num5.ToString());
+            //MessageBox.Show(date.ToShortDateString().ToString());
 
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
 
         }
     }
